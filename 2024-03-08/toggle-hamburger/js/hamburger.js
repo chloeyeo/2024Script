7b028f24d5fd46047ee89547ca0d4d8a41c139ba
Chloe Yeo <yeochloe01@gmail.com>
2024-03-08 16:21:22 +0900

    made header and slider of a mock sansumgsem.com website

--- 2024-03-08/toggle-hamburger/js/hamburger.js
+++ 2024-03-08/toggle-hamburger/js/hamburger.js
@@ -11,7 +11,7 @@ document.querySelectorAll(".toggle-button")[0].classList.remove('active')*/
 
 const toggleBtn = document.getElementsByClassName('toggle-button')[0];
 
-let opened = toggleBtn.classList.contains('active') ? true : false;
+// let opened = toggleBtn.classList.contains('active') ? true : false;
 
 // function toggle() {
 //     if (!opened) {
@@ -29,5 +29,9 @@ toggleBtn.addEventListener("click", function(){
     toggleBtn.classList.toggle("active"); /* this code is the same as above!!
     toggle("active") adds "active" to toggleBtn.classList if it doesn't already exist in the list,
     and removes "active" from the classList if it does already exist in the list.
-    and "click" even means on click do this toggle("active") */
+    and "click" even means on click do this toggle("active") 
+
+    This is the DOMTokenList: toggle() method. 
+    The toggle() method of the DOMTokenList interface removes an existing token from the list and returns false.
+    If the token doesn't exist it's added and the function returns true.*/
 });
