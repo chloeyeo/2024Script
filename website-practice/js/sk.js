6f200147431ed34e9132345ab561aeace0f68000
Chloe Yeo <yeochloe01@gmail.com>
2024-03-11 19:38:53 +0900

    change play/pause buttons to each other on click

--- website-practice/js/sk.js
+++ website-practice/js/sk.js
@@ -1,7 +1,11 @@
 const slides = document.querySelectorAll(".slide");
 const progressBars = document.querySelectorAll(".indicator .bar");
+const numIcons = document.querySelectorAll(".indicator span");
 let currIndex = -1;
 let timeout = null;
+
+// document.getElementsByClassName("play-btn")[0].children[0].classList.contains("fa-pause")
+// document.getElementsByClassName("play-btn")[0].children[0].classList.replace("fa-pause","fa-play")
 
 function autoSlideNext() {
   timeout = setTimeout(autoSlideNext, 3000);
@@ -9,6 +13,7 @@ function autoSlideNext() {
     slides[i].style.display = "none";
     progressBars[i].style.display = "none";
     progressBars[i].classList.remove("active");
+    numIcons[i].classList.remove("active");
   }
   if (currIndex == slides.length - 1) {
     currIndex = 0;
@@ -18,6 +23,7 @@ function autoSlideNext() {
   slides[currIndex].style.display = "block";
   progressBars[currIndex].style.display = "block";
   progressBars[currIndex].classList.add("active");
+  numIcons[currIndex].classList.add("active");
 }
 
 autoSlideNext();
@@ -31,4 +37,13 @@ function changeSlide(currIndicator) {
 
   /* this calls the autoSlideNext function AFTER a delay of 3000 milliseconds */
   timeout = setTimeout(autoSlideNext, 3000);
+}
+
+function changePause(playBtn) {
+  let iconClassList = playBtn.children[0].classList;
+  if (iconClassList.contains("fa-play")) {
+    iconClassList.replace("fa-play", "fa-pause");
+  } else {
+    iconClassList.replace("fa-pause", "fa-play");
+  }
 }
