4935bd5665fdf60ef64cf1219408b0505962b482
Chloe Yeo <yeochloe01@gmail.com>
2024-03-14 19:32:43 +0900

    crud functions of todo-list working using window localStorage as db

--- todo-list/js/index.js
+++ todo-list/js/index.js
@@ -36,13 +36,13 @@ function listDataObjects() {
   let text = "";
   for (let i = 0; i < listData.length; i++) {
     /* <li class="active"> */
-    text += `<li><div class='listed-item' onClick="highlightItem(${i})">${listData[i].title}</div>`;
+    text += `<li><div class='listed-item'>${listData[i].title}</div><a onClick="checkItem(${i})">`;
     if (listData[i].checked) {
-      text += `<a onClick="checkItem(${i})"><i class="fa-regular fa-square-check"></i></a>`;
+      text += `<i class="fa-regular fa-square-check"></i>`;
     } else {
-      text += `<a onClick="checkItem(${i})"><i class="fa-regular fa-square"></i></a>`;
+      text += `<i class="fa-regular fa-square"></i>`;
     }
-    text += ` <div class="del-btn" onclick="deleteSpecificElement(${i})"><i class="fa-solid fa-trash"></i></div></li>`;
+    text += `</a><div class="del-btn" onclick="deleteSpecificElement(${i})"><i class="fa-solid fa-trash"></i></div></li>`;
   }
   viewListElem.innerHTML = text;
 }
@@ -65,6 +65,7 @@ function checkItem(index) {
   // and re-update the view/ui based on updated database
   listDataObjects();
   */
+  highlightItem(index);
   let classList = checkItems[index].children[0].classList;
   if (classList.contains("fa-square-check")) {
     classList.replace("fa-square-check", "fa-square");
@@ -82,27 +83,40 @@ function highlightItem(index) {
 }
 
 function listElements() {
-  /* get data from localStorage and put it in viewList.
-  if (localStorage.length > 0){
-    let localData = localStorage.getItem("todoData");
-    console.log(localData);
-    // localData is JSON data but our data isn't
-    // so we have to convert JSON data back to local data format
-    // using JSON.parse (= opposite of JSON.stringify)
-    // before we parse the data, our code won't recognise
-    // the JSON and will just treat it as a string of text just text
-    // only after JSON.parse it will convert JSON back to local data
-    // and our code will recognise it as what it is, an object
-    console.log(JSON.parse(localData));
-    viewList = JSON.parse(localData);
+  //  get data from localStorage and put it in viewList.
+  // localData is JSON data but our data isn't
+  // so we have to convert JSON data back to local data format
+  // using JSON.parse (= opposite of JSON.stringify)
+  // before we parse the data, our code won't recognise
+  // the JSON and will just treat it as a string of text just text
+  // only after JSON.parse it will convert JSON back to local data
+  // and our code will recognise it as what it is, an object
+  if (localStorage.length > 0) {
+    let storedData = localStorage.getItem("todoData");
+    console.log(`This is JSON todoData: ${storedData}`);
+    viewList = JSON.parse(storedData);
+    console.log(`This is parsed local todoData: ${viewList}`);
   }
-  */
+
   let textList = "";
-  if (viewList.length > 0) {
-    for (let i = 0; i < viewList.length; i++) {
-      textList += `<li><div class='listed-item'>${viewList[i].title}</div> <div class="del-btn" onclick="deleteSpecificElement(${i})"><i class="fa-solid fa-trash"></i></div></li>`;
+  for (let i = 0; i < viewList.length; i++) {
+    /* <li class="active"> */
+    // console.log(
+    //   `viewList[${i}].title: ${viewList[i].title}, viewList[${i}].checked: ${viewList[i].checked}`
+    // );
+    textList += `<li><div class='listed-item'>${viewList[i].title}</div><a onClick="checkItem(${i})">`;
+    if (viewList[i].checked) {
+      textList += `<i class="fa-regular fa-square-check"></i>`;
+    } else {
+      textList += `<i class="fa-regular fa-square"></i>`;
     }
-  } else {
+    textList += `</a><div class="del-btn" onclick="deleteSpecificElement(${i})"><i class="fa-solid fa-trash"></i></div></li>`;
+  }
+
+  // for (item of viewList) {
+  //   textList += `<li><div class='listed-item'>${viewList[i].title}</div> <div class="del-btn" onclick="deleteSpecificElement(${i})"><i class="fa-solid fa-trash"></i></div></li>`;
+  // }
+  if (viewList == []) {
     textList = `<li><div>Nothing to do today!</div></li>`;
   }
   viewListElem.innerHTML = textList;
@@ -117,64 +131,57 @@ function deleteAll() {
 // checkbox => check/uncheck is equivalent to modifying data
 
 function addElement() {
+  //todoElem = input val
   if (todoElem.value != "") {
-    /* let value = { title: todoElem.value, check: false };
+    let value = { title: todoElem.value, checked: false };
     viewList.push(value);
-    we also have to put value into windows.localstorage as
-    JSON data using stringify
-    stringify will convert value = {} into JSON
-    so that we store the JSON in windows.localstorage
-    
-    convert to JSON data. JSON data has fields as strings too.
+    // we also have to put value into windows.localstorage as
+    // JSON data using stringify
+    // stringify will convert value = {} into JSON
+    // so that we store the JSON in windows.localstorage
+
+    // convert to JSON data. JSON data has fields as strings too.
     let valueData = JSON.stringify(viewList);
-    console.log(valueData);
+    console.log(
+      `this is the stringified JSON data of viewList width added item: ${valueData}`
+    );
 
     localStorage.setItem("todoData", valueData); // update localStorage
 
-    when reload page, the listed items in UI will disappear
-    but what we stored in localStorage will still remain
+    // when reload page, the listed items in UI will disappear
+    // but what we stored in localStorage will still remain
 
-    so then we get data from windows.localStorage
-    and put this data into viewList.
-    */
+    // so then we get data from windows.localStorage
+    // and put this data into viewList.
 
-    viewList.push(todoElem.value);
+    // viewList.push(todoElem.value);
     todoElem.value = "";
-  }
-  /*else {
-    alert("오늘의 할일을 입력하세요");
+  } else {
+    alert("Please type in what to do today");
     todoElem.focus(); // = input bar 깜빡깜빡 effect
-  } */
-  listElements();
+  } // The focused element is the element that will receive keyboard and similar events by default
+  listElements(); //update ui/ show list in view
   // listDataObjects();
 }
 
-function deleteElement() {
-  viewList.pop();
-  if (viewList.length > 0) {
-    listElements();
-  } else {
-    viewListElem.innerHTML = "";
-  }
-}
+// function deleteElement() {
+//   viewList.pop();
+//   if (viewList.length > 0) {
+//     listElements();
+//   } else {
+//     viewListElem.innerHTML = "";
+//   }
+// }
 
 function deleteSpecificElement(index) {
-  listData.splice(index, 1); // splice removes 1 element in viewList at index
-  /* let valueData = JSON.stringify(listData);
-  localStorage.setItem("todoData", valueData); // update database
-  
-  listDataObjects(); // update view (from updated database)
-  
-  */
-  console.log(listData);
-  if (listData.length > 0) {
-    listDataObjects(); // update view after deletion
-  } else {
-    viewListElem.innerHTML = "";
-  }
+  viewList.splice(index, 1); // splice removes 1 element in viewList at index
+  let jsonData = JSON.stringify(viewList);
+  localStorage.setItem("todoData", jsonData); // update database
+
+  listElements(); // update view (from updated database)
 }
 
-listDataObjects();
+// listDataObjects();
 
 // addElement and deleteAll are callback functions
 // i.e. they are only called 'on click'
